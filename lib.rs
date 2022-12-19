4882f8209e30288aa7379b884db687ca30525c76
stackinspector <stackinspector@126.com>
2022-12-19 18:46:48 +0800

    features

--- lib.rs
+++ lib.rs
@@ -1,23 +1,29 @@
-#[cfg(feature = "concat-string")]
-mod concat_string;
-#[cfg(feature = "error-enum")]
-mod error_enum;
+#[cfg(feature = "now")]
+pub mod now;
+#[cfg(feature = "usize-casting")]
+pub mod usize_casting;
 #[cfg(feature = "byterepr")]
 pub mod byterepr;
-#[cfg(feature = "byterepr-macros")]
-mod byterepr_macros;
-#[cfg(feature = "kvdump")]
-pub mod kvdump;
+#[cfg(feature = "bytes-read")]
+pub mod bytes_read;
+#[cfg(feature = "num-compress")]
+pub mod num_compress;
+#[cfg(feature = "case-convert")]
+pub mod case_convert;
+#[cfg(feature = "const-bytes-equal")]
+pub mod const_bytes_equal;
 #[cfg(feature = "key-index")]
 pub mod key_index;
 #[cfg(feature = "key-index-map")]
 pub mod key_index_map;
-
-pub mod now;
-pub mod usize_casting;
-pub mod num_compress;
-pub mod case_convert;
-pub mod const_bytes_equal;
-pub mod bytes_read;
-
+#[cfg(feature = "sha3")]
 pub mod sha3;
+#[cfg(feature = "kvdump")]
+pub mod kvdump;
+
+#[cfg(feature = "concat-string")]
+mod concat_string;
+#[cfg(feature = "error-enum")]
+mod error_enum;
+#[cfg(feature = "byterepr-macros")]
+mod byterepr_macros;
