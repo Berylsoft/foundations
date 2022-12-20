47e7657ac4e5b5627af3a63dc184ecb89d9585a2
stackinspector <stackinspector@126.com>
2022-12-20 15:21:24 +0800

    sha3: more once

--- sha3.rs
+++ sha3.rs
@@ -7,3 +7,19 @@ pub fn shake256_once<const N: usize>(bytes: &[u8]) -> [u8; N] {
     hasher.squeeze(&mut res);
     res
 }
+
+pub fn sha3_256_once(bytes: &[u8]) -> [u8; 32] {
+    let mut hasher = Sha3::v256();
+    hasher.update(bytes);
+    let mut res = [0; 32];
+    hasher.finalize(&mut res);
+    res
+}
+
+pub fn sha3_512_once(bytes: &[u8]) -> [u8; 64] {
+    let mut hasher = Sha3::v512();
+    hasher.update(bytes);
+    let mut res = [0; 64];
+    hasher.finalize(&mut res);
+    res
+}
