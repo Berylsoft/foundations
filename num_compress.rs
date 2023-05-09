ec23e1751c51bb6fa68b8737febabb112fbf01b3
stackinspector <stackinspector@126.com>
2023-05-09 12:05:25 +0800

    num_compress: trailing_zero_byte leading_zero_byte

--- num_compress.rs
+++ num_compress.rs
@@ -9,10 +9,17 @@ pub const fn zigzag_decode(n: u64) -> i64 {
 }
 
 #[inline]
-pub fn float_find_zero(f: u64) -> usize {
+pub fn trailing_zero_byte(n: u64) -> usize {
     // after dividing by 8, trailing bytes in this amount must be all 0,
     // because the remainder is the trailing zero of last byte that is not 0.
-    (8 - (f.trailing_zeros() / 8)) as usize
+    (8 - (n.trailing_zeros() / 8)) as usize
+}
+
+#[inline]
+pub fn leading_zero_byte(n: u64) -> usize {
+    // after dividing by 8, leading bytes in this amount must be all 0,
+    // because the remainder is the leading zero of last byte that is not 0.
+    (8 - (n.leading_zeros() / 8)) as usize
 }
 
 #[inline]
@@ -33,10 +40,16 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!(float_find_zero(0x00_00_00_00_00_00_00_00), 0);
-        assert_eq!(float_find_zero(0x01_00_00_00_00_00_00_00), 1);
-        assert_eq!(float_find_zero(0x01_00_00_01_00_00_00_00), 4);
-        assert_eq!(float_find_zero(0x01_00_00_01_10_00_00_00), 5);
-        assert_eq!(float_find_zero(0x01_00_00_01_00_00_00_01), 8);
+        assert_eq!(trailing_zero_byte(0x00_00_00_00_00_00_00_00), 0);
+        assert_eq!(trailing_zero_byte(0x01_00_00_00_00_00_00_00), 1);
+        assert_eq!(trailing_zero_byte(0x01_00_00_01_00_00_00_00), 4);
+        assert_eq!(trailing_zero_byte(0x01_00_00_01_10_00_00_00), 5);
+        assert_eq!(trailing_zero_byte(0x01_00_00_01_00_00_00_01), 8);
+
+        assert_eq!(leading_zero_byte(0x00_00_00_00_00_00_00_00), 0);
+        assert_eq!(leading_zero_byte(0x00_00_00_00_00_00_00_01), 1);
+        assert_eq!(leading_zero_byte(0x00_00_00_00_01_00_00_01), 4);
+        assert_eq!(leading_zero_byte(0x00_00_00_10_01_00_00_01), 5);
+        assert_eq!(leading_zero_byte(0x01_00_00_00_01_00_00_01), 8);
     }
 }
