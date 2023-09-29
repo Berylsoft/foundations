9382db8b0dc82d954ee2f4e991d27628409585c5
stackinspector <stackinspector@126.com>
2023-09-30 01:51:33 +0800

    num-enum: unreachable

--- num_enum.rs
+++ num_enum.rs
@@ -4,6 +4,29 @@ macro_rules! num_enum {
         $(#[$meta:meta])*
         $pub:vis enum $name:ident {
             $($variant:ident = $n:literal,)*
+        } as $num:ident else unreachable
+    } => {
+        #[repr($num)]
+        #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+        $(#[$meta])*
+        $pub enum $name {
+            $($variant = $n,)*
+        }
+
+        impl From<$num> for $name {
+            fn from(n: $num) -> $name {
+                match n {
+                    $($n => $name::$variant,)*
+                    _ => unreachable!(),
+                }
+            }
+        }
+    };
+
+    {
+        $(#[$meta:meta])*
+        $pub:vis enum $name:ident {
+            $($variant:ident = $n:literal,)*
         } as $num:ident else $error:ident::$error_variant:ident
     } => {
         #[repr($num)]
@@ -15,7 +38,7 @@ macro_rules! num_enum {
 
         impl TryFrom<$num> for $name {
             type Error = $error;
-            
+
             fn try_from(n: $num) -> std::result::Result<$name, $error> {
                 match n {
                     $($n => Ok($name::$variant),)*
