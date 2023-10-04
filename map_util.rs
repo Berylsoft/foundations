9a528bfb754070051e73f04dba8263f1819c684a
stackinspector <stackinspector@126.com>
2023-10-05 03:00:30 +0800

    map-util: remove get_or_insert

--- map_util.rs
+++ map_util.rs
@@ -1,12 +1,6 @@
 use std::collections::{BTreeMap, btree_map::Entry};
 
-pub fn get_or_insert<K: Ord, V, F: FnOnce() -> V>(map: &mut BTreeMap<K, V>, k: K, init_v: F) -> &mut V {
-    match map.entry(k) {
-        Entry::Vacant(entry) => entry.insert(init_v()),
-        Entry::Occupied(entry) => entry.into_mut(),
-    }
-}
-
+// remove when map_try_insert stable
 pub fn first_insert<K: Ord, V>(map: &mut BTreeMap<K, V>, k: K, v: V) -> &mut V {
     match map.entry(k) {
         Entry::Vacant(entry) => entry.insert(v),
